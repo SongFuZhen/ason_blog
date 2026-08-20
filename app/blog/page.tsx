a4104453b7be9193b98fa36d47bd6d5a1f9fffbe
SongFuZhen <evil_song@126.com>
2026-08-20 23:20:31 +0800

    refactor(blog): remove pagination, list all posts on one page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,8 +3,6 @@ import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
 import ListLayout from '@/layouts/ListLayoutWithTags'
 
-const POSTS_PER_PAGE = 5
-
 export const metadata = genPageMetadata({
   title: '文章',
   description:
@@ -13,20 +11,6 @@ export const metadata = genPageMetadata({
 
 export default async function BlogPage() {
   const posts = allCoreContent(sortPosts(allBlogs))
-  const pageNumber = 1
-  const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE)
-  const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE * pageNumber)
-  const pagination = {
-    currentPage: pageNumber,
-    totalPages,
-  }
 
-  return (
-    <ListLayout
-      posts={posts}
-      initialDisplayPosts={initialDisplayPosts}
-      pagination={pagination}
-      variant="blog"
-    />
-  )
+  return <ListLayout posts={posts} initialDisplayPosts={posts} variant="blog" />
 }
